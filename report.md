a1f85e52578ce348026331575ef9234d5f455139
ChuaKhangHui <chuakhanghui@1utar.my>
2016-03-11 00:21:30 +0800

    Added files via upload
    upload 3/11 12:18

--- report.md
+++ report.md
@@ -14,24 +14,60 @@ The reports, codes and supporting documents are to be uploaded to Github at:
 
 Explain your selection criteria here.
 
+1. Check strictly diagonally dominant Matrix
+	if (Matrix A is strictly diagonally dominant)
+		then Solve it by LU
+	else
+		cotinue to 2
+2. Check possitive Definite 
+	if (Matrix A is possitive definite and all diagonal element are possitive)
+		then Solve it by SOR
+	else
+		Solve it by LU
+		
 Explain how you implement your `task1.py` here.
+
+Matrix A is classified as strictly diagonally dominant matrix when (2* diagonal element > sum of row) for all rows.
+
+In python: 
+	temp = 2 * np.diag(A) > np.sum(np.abs(A),1)
+    result = temp.all()
+If the result is true, then A is a strictly diagonally dominant Matrix
+
+Matrix A is classified as strictly possitive definite matrix when  np.linalg.cholesky(A) return no error.
+
+If error received, slove it by LU.
+else no error received, then check for possitive diagonal element.
+If all element is possitive, then we solve it by SOR, else, slove it by LU.
+
 
 ---------------------------------------------------------
 
 ## Task 2 -- SVD method and image compression
 
-Put here your picture file (Lenna.png)
+Put here your picture file (KB.png)
 
-![Lenna.png](Lenna.png)
+![KB.png](KB.png)
 
 How many non zero element in $\Sigma$?
+For am NxM sized image, the number of zeros in $\Sigma$ will be (N $\times$ M - N) 
 
 Put here your lower and better resolution pictures. Explain how you generate
 these pictures from `task2.py`.
 
-What is a sparse matrix?
+The image with lower resolution
+![KB_lower.png](KB_lower.png)
 
+The image with better resolution
+![KB_better.png](KB_better.png)
+
+First, recaculate a new $\Sigma$ with differnt number of eigenvector used, 30 for lower resolution and 200 for better resolution.
+Both picture is then obtain by compute the matrix by U $?\Sigma$ V for each color layer, then merge togehter to become an image in RGB format.
+
+What is a sparse matrix?
+Sparse matrix is the matrix with high percentage of zero entries.
+In this assignment, $\Sigma_n$ is the example of sparse matrix which contain only n non-zero element out of NxM element.
 
 -----------------------------------
 
-<sup>last modified: change your date here</sup>
+<sup>last modified: 3/11/2016</sup>
--- task1.py
+++ task1.py
@@ -1,19 +1,79 @@
 import numpy as np
 #Your optional code here
 #You can import some modules or create additional functions
+import scipy.linalg as la
 
 def lu(A, b):
-    sol = []
+    #sol = []
+    
     # Edit here to implement your code
-    return list(sol)
+    
+    #p,L,U = la.lu(A)
+    L,U = la.lu(A,True)
+    #print (p)
+    #print (L)
+    #print(U)
+    
+    #ly = b
+    #Ux = y
+    y = la.solve(L,b)
+    #print(y)
+    x = la.solve(U,y)
+    #print (x)
+    
+    #check ans, Ax = b
+    #print (np.dot(A,x))
+
+    return list(x)
 
 def sor(A, b):
-    sol = []
+    #sol = []
     # Edit here to implement your code
-    return list(sol)
+    
+    ITERATION_LIMIT = 100  
+    size = len(A)
+    
+    # A = D - L - U
+    D = np.zeros_like(A)
+    for i in range(size):
+        D[i][i] = A[i][i]
+
+    L = np.zeros_like(A)
+    for i in range(size):
+        for j in range(i):
+            L[i][j] = -A[i][j]
+    
+    U = np.zeros_like(A)
+    for i in range(size):
+        for j in range(i+1,size):
+            U[i][j] = -A[i][j]
+    
+    #omega
+    K = np.zeros_like(A)
+    for i in range(size):
+        K[i][i] = 1/D[i][i]
+        
+    eig = la.eigvals(K)
+    p = max(abs(eig))    
+    p2 = np.power(p,2)
+    
+    omega = 2 * (1 - np.sqrt(1 - p2)) / p2    
+    #print ('omega = ' ,omega)
+
+    if (omega <= 0 or omega >= 2.0):
+        omega = 0.9
+        
+    Q = D/omega -L
+    K = np.linalg.inv(Q).dot(Q-A)
+    c = np.linalg.inv(Q).dot(b)
+    x = np.zeros_like(b)
+    
+    for itr in range(ITERATION_LIMIT):
+        x    = K.dot(x) + c   
+    return list(x)
 
 def solve(A, b):
-    condition = True # State and implement your condition here
+    condition = check_condition(A) # State and implement your condition here
     if condition:
         print('Solve by lu(A,b)')
         return lu(A,b)
@@ -21,6 +81,37 @@ def solve(A, b):
         print('Solve by sor(A,b)')
         return sor(A,b)
 
+
+
+def check_condition(A):
+#condition
+    try:
+        #Check Strictly Diagonally dominant Matrix
+        temp = 2 * np.diag(A) > np.sum(np.abs(A),1)
+        result = temp.all()
+        
+        if (result):
+            #Is Strictly Diagonally dominant Matrix
+            #Slove by Lu
+            return True
+            
+        #Check possitive Definite Matrix
+        np.linalg.cholesky(A)
+        
+        #possitive diaganal element
+        temp = np.diag(A) > 0
+        result = temp.all()
+        if (~result):
+            #some diagonal element is negative
+            retrun True
+        
+    except np.linalg.linalg.LinAlgError :
+        #'Solve by lu'
+        return True
+    return False
+
+
+ 
 if __name__ == "__main__":
     ## import checker
     ## checker.test(lu, sor, solve)
